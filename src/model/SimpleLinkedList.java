2ccbcbab36556cbefac9958547b970f81fcb4550
Johan Daniel Aguirre Arias <1110290182@u.icesi.edu.co>
2023-02-10 08:33:03 -0500

    se agrega la linkedlist junto con su main

---
+++ src/model/SimpleLinkedList.java
@@ -0,0 +1,62 @@
+public class SimpleLinkedList {
+
+    private Node root;
+
+    public SimpleLinkedList() {
+        this.root = null;
+    }
+
+    public void addNodeAtEnd(Node pointer, int value){
+        Node node = new Node( value);
+        if(root==null){ // caso base, lista vacía
+            root=node;
+        }
+        else if(pointer.getNext()==null){ // caso base, se encontro el ultimo
+            pointer.setNext(node);
+        } else { // se sigue buscando el ultimo recursivamente
+            addNodeAtEnd(pointer.getNext(), value);
+        }
+    }
+
+    public void addNodeAfterTarget(Node pointer, int value, int targetValue) {
+        if(pointer!=null) { // caso base, lista vacía o no se encontro el target
+            if(pointer.getValue()==targetValue){ // se encontro el target
+                Node node = new Node( value);
+                node.setNext(pointer.getNext());
+                pointer.setNext(node);
+            } else { // se sigue buscando el target recursivamente
+                addNodeAfterTarget(pointer.getNext(), value, targetValue);
+            }
+        }
+    }
+
+	public void deleteNode(Node pointer, int targetValue){
+		if(pointer!=null){
+			if(root.getValue() == targetValue){
+				root=pointer.getNext();
+			}
+			else if(pointer.getNext().getValue()==targetValue ){
+				Node  pointer1=pointer.getNext();
+				pointer.setNext(pointer1.getNext());
+			}else{
+				deleteNode(pointer.getNext(),targetValue);
+			}
+		}
+	}
+	
+	
+    public void getList(Node pointer){
+        if(pointer!=null){
+            System.out.println(pointer.getValue());
+            getList(pointer.getNext());
+        }
+
+    }
+
+
+    public Node getRoot() {
+        return root;
+    }
+
+	
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    static private SimpleLinkedList simpleLinkedList;
+    public static void main(String[] args) {
+        simpleLinkedList = new SimpleLinkedList();
+
+        simpleLinkedList.getList(simpleLinkedList.getRoot());
+
+       simpleLinkedList.addNodeAtEnd(simpleLinkedList.getRoot(), 10);
+        simpleLinkedList.addNodeAtEnd(simpleLinkedList.getRoot(), 20);
+        simpleLinkedList.addNodeAfterTarget(simpleLinkedList.getRoot(), 15, 10);
+        simpleLinkedList.addNodeAtEnd(simpleLinkedList.getRoot(), 50);
+        simpleLinkedList.addNodeAtEnd(simpleLinkedList.getRoot(), 100);
+		 simpleLinkedList.getList(simpleLinkedList.getRoot());
+		 System.out.println("  change ");
+		simpleLinkedList.deleteNode(simpleLinkedList.getRoot(), 100);
+        simpleLinkedList.getList(simpleLinkedList.getRoot());
+    }
+}
